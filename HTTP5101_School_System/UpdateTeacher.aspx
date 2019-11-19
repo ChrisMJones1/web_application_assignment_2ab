4e91f6081b4fd9aab33df0ea04b2c9a58c476376
ella.qi <ella.qishuyan@gmail.com>
2019-11-19 07:19:31 -0500

    Update Teacher
    Update Teacher

---
+++ HTTP5101_School_System/UpdateTeacher.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UpdateTeacher.aspx.cs" Inherits="HTTP5101_School_System.UpdateTeacher" %>
+<asp:Content ID="teachers_list" ContentPlaceHolderID="body" runat="server">
+     <script type="text/javascript" src="updatestudents_javascript.js" ></script>
+    <!-- <style>#firstname_change{display: none;}</style> -->
+    <div id="updating_teachers" runat="server">
+        <h2>Updating a teacher information: <span id="teacher_fullname" runat="server"></span></h2>
+
+        <div>Current first name: <span id="teacher_fname" runat="server"></span>
+            <div id="firstname_update">
+                <label for="update_teacherfname">New first name:</label> 
+                <asp:Textbox runat="server" ID="update_teacherfname"></asp:Textbox>
+            </div>
+        </div>
+
+        <div> Current last name: <span id="teacher_lname" runat="server"></span>
+            <div id="lastname_update">
+               <label for ="update_teacherlname">New last name:</label>
+               <asp:Textbox runat="server" ID="update_teacherlname"></asp:Textbox>
+            </div>
+        </div>
+
+         <div>Current employee number: <span id="teacher_employeenumber" runat="server"></span>
+            <div id="employeenumber_update">
+                 <label for="update_employeenumber"> New employee number:</label>
+                 <asp:Textbox runat="server" ID="update_employeenumber"></asp:Textbox>
+                 <asp:RegularExpressionValidator runat="server" ControlToValidate="update_employeenumber" EnableClientScript="true" ErrorMessage="Please enter the right format of employee number" ValidationExpression="^T|n[0-9]{3}$"></asp:RegularExpressionValidator>
+            </div>
+        </div>
+
+        <div> Current hire date: <span id="teacher_hiredate" runat="server"></span>
+            <div id="hiredate_update">
+                <label for ="update_hiredate"> New hire date:</label>
+                <asp:Textbox runat="server" ID="update_hiredate"></asp:Textbox>
+            </div>
+        </div>
+
+        <div> Current salary: <span id="teacher_salary" runat="server"></span>
+            <div id="salary_update">
+                <label for ="update_salary"> New salary:</label>
+                <asp:Textbox runat="server" ID="update_salary"></asp:Textbox>
+                <asp:RegularExpressionValidator runat="server" ControlToValidate="update_salary" EnableClientScript="true" ErrorMessage="Please enter the right format of salary" ValidationExpression="^[0-9]*(\.[0-9]{1,2})?$"></asp:RegularExpressionValidator>
+            </div>
+        </div>
+      
+        <asp:Button runat="server" text="Updating" />
+        <div id="update_summary" runat="server"></div>
+    </div>
+
+</asp:Content>
---
+++ HTTP5101_School_System/UpdateTeacher.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HTTP5101_School_System
+{
+    public partial class UpdateTeacher : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            bool valid = true;
+            string teacherid = Request.QueryString["teacherid"];
+            if (String.IsNullOrEmpty(teacherid)) valid = false;
+
+            if (valid)
+            {
+                var db = new SCHOOLDB();
+                Dictionary<String, String> teacher_record = db.FindTeacher(Int32.Parse(teacherid));
+
+                if (teacher_record.Count > 0)
+                {
+                    teacher_fullname.InnerHtml = teacher_record["TEACHERFNAME"] + " " + teacher_record["TEACHERLNAME"];
+                    teacher_fname.InnerHtml = teacher_record["TEACHERFNAME"];
+                    teacher_lname.InnerHtml = teacher_record["TEACHERLNAME"];
+                    teacher_employeenumber.InnerHtml = teacher_record["EMPLOYEENUMBER"];
+                    teacher_hiredate.InnerHtml = teacher_record["HIREDATE"];
+                    teacher_salary.InnerHtml = teacher_record["SALARY"];
+                }
+                else
+                {
+                    valid = false;
+                }
+                if (Page.IsPostBack)
+                {
+                    Page.Validate();
+                    if (Page.IsValid)
+                    {
+
+                        string Update_Teacherfname = update_teacherfname.Text.ToString();
+                        string Update_Teacherlname = update_teacherlname.Text.ToString();
+                        string Update_Employeenumber = update_employeenumber.Text.ToString();
+                        string Update_Hiredate = update_hiredate.Text.ToString();
+                        string Update_Salary = update_salary.Text.ToString();
+
+                        update_summary.InnerHtml = "You have changed the following information about " + teacher_fullname.InnerHtml + ":<br>";
+
+                        if (Update_Teacherfname != "")
+                        {
+                            update_summary.InnerHtml += "First name: " + Update_Teacherfname + "<br>";
+                        }
+                        else
+                        {
+                            update_summary.InnerHtml += "First name unchanged <br>";
+                        }
+                        if (Update_Teacherlname != "")
+                        {
+                            update_summary.InnerHtml += "Last name: " + Update_Teacherlname + "<br>";
+                        }
+                        else
+                        {
+                            update_summary.InnerHtml += "Last name unchanged <br>";
+                        }
+                        if (Update_Employeenumber != "")
+                        {
+                            update_summary.InnerHtml += "Employee number: " + Update_Employeenumber + "<br>";
+                        }
+                        else
+                        {
+                            update_summary.InnerHtml += "Employee number unchanged <br>";
+                        }
+                        if (Update_Hiredate != "")
+                        {
+                            update_summary.InnerHtml += "Hire date: " + Update_Hiredate + "<br>";
+                        }
+                        else
+                        {
+                            update_summary.InnerHtml += "Hire date unchanged <br>";
+                        }
+                        if (Update_Salary != "")
+                        {
+                            update_summary.InnerHtml += "Salary: " + Update_Salary + "<br>";
+                        }
+                        else
+                        {
+                            update_summary.InnerHtml += "Salary unchanged <br>";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
